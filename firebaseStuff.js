8f770d5bb5db897574a3cca66973d5ff6239403c
juliauhr <48628479+juliauhr@users.noreply.github.com>
2020-11-28 14:49:23 -0700

    Add files via upload

--- firebaseStuff.js
+++ firebaseStuff.js
@@ -3,7 +3,7 @@ var database = firebase.firestore();
 //var docRef = database.collection("users").doc("julia");
 var provider = new firebase.auth.GoogleAuthProvider();
 var message = document.getElementById("message");
-var current_user = null;
+var currentUser = null;
 
 function authenticate(){
 	firebase.auth().getRedirectResult().then(function(result) {
@@ -13,8 +13,8 @@ function authenticate(){
 	  else {
 		  firebase.auth().signInWithPopup(provider);
 	  }
-	  current_user = result.user;
-	  message.innerHTML = "Signed in as " + current_user.email;
+	  currentUser = result.user;
+	  message.innerHTML = "Signed in as " + currentUser.email;
 	}).catch(function(error) {
 		console.log('error signing in: ', error);
 	});
@@ -31,19 +31,17 @@ function signOut(){
 
 firebase.auth().onAuthStateChanged(function(user){
 	console.log("auth state changed");
-	current_user=user;
+	currentUser=user;
 	if(user){
 		document.querySelector('#saveButton').style.display = 'inline';
 		message.innerHTML = "Signed in as " + user.email;
-
-		docRef = database.collection("users").doc("julia");
+		docRef = database.collection("users").doc(currentUser);
 		docRef.get().then(function(doc) {
 	    if (doc.exists) {
-        //console.log("Document data:", doc.data());
 				text = doc.data().text;
 				updateText();
 	    }else{
-	      console.log("No such document!");
+	      //console.log("No such document!");
 	    }
 			}).catch(function(error) {
 		    console.log("Error getting document:", error);
@@ -53,14 +51,13 @@ firebase.auth().onAuthStateChanged(function(user){
   	}
 });
 
-
 function saveText(){
 	var txt = document.querySelector('#flatText').value;
-	database.collection("users").doc(current_user.email).set({
+	database.collection("users").doc(currentUser.email).set({
     text: txt
 	})
 	.then(function() {
-	    console.log("Document written: "+txt);
+	    console.log("Document written");
 	})
 	.catch(function(error) {
 	    console.error("Error writing document: ", error);
